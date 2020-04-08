cfed5ed18e7e82e4406415a31a8880bb7c39e6c1
Sean Riley <sean.riley@sxi.co.za>
2020-04-08 10:18:30 +0200

    Removed unneccessary files and cleaned up paths in the docker-compose files

--- sqlexpress/StartUp.cmd
+++
@@ -1,1 +0,0 @@
-"c:\Program Files\java\jdk-13.0.2\bin\java" -jar D:\code\dev-sxi\v3.0\trunk\xlayer-api\target\sxirestapi-3.0-SNAPSHOT.jar --spring.config.location=file:///d:/dockerImgCfgs/xlayerrestapi_sqlexpress/application.properties
