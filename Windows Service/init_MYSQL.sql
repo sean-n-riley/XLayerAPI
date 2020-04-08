e5392f28c0ed3243de3377d8bdc1913918672106
Sean Riley <sean.riley@sxi.co.za>
2020-04-08 08:48:01 +0200

    Added Files needed to install as windows service

---
+++ Windows Service/init_MYSQL.sql
@@ -0,0 +1,3 @@
+create database if not exists sxirestapi 
+    character set = 'utf8' 
+    collate = 'utf8_general_ci';
---
+++ Windows Service/readme.md
@@ -0,0 +1,1 @@
+NOTE: you will need the XLayerAPI.jar file to install and run this as a windows service.
