5782ebe02eca8876c9c3a478253c2f4df7843507
Sean Riley <sean.riley@sxi.co.za>
2020-04-08 11:36:27 +0200

    Added simpe batch files to start all the docker containers needed for the test easily

--- LaunchXLayerAPI.cmd
+++ LaunchXLayerAPI.cmd
@@ -1,17 +1,33 @@
 @echo off
+CLS
+:MENU
 echo.
-echo Starting MariaDB Docker Container
+echo =================================
+echo  Press 1,2 to select which
+echo  database container to start 
+echo  or press 3 to EXIT
 echo =================================
 echo.
+echo 1 - MariaDB
+echo 2 - MS SQL Express
+echo 3 - EXIT
+echo.
+SET /P M=Type 1, 2, or 3 then press ENTER:
+IF %M%==1 GOTO MARIADB
+IF %M%==2 GOTO MSSQLEXPRESS
+IF %M%==3 GOTO EOF
+
+:MARIADB
 echo Starting MariaDB Docker
 start /D D:\code\XLayerAPI\mariadb\ StartMariaDB.cmd
-echo.
-REM echo Starting MSSQLExpress Docker
-REM start /D D:\code\XLayerAPI\sqlexpress\ StartSQLExpressa.cmd
-REM echo.
+goto KAFKA
+
+:MSSQLEXPRESS
+echo Starting MSSQLExpress Docker
+start /D D:\code\XLayerAPI\sqlexpress\ StartSQLExpressa.cmd
+goto KAFKA
+
+:KAFKA
 echo Starting Kafka Docker
 start /D D:\code\XLayerAPI\kafka\ StartKafka.cmd
-echo.
-echo =================================
-
-pause
+GOTO EOF
