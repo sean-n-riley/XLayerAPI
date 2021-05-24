3c1243f5476d34acc601e3c663f3b6da26a95e93
sean.riley@sxi.co.za <T@gi2b2@321>
2021-05-24 16:35:34 +0200

    New v3.3P5

--- WindowsService/StartUp.cmd
+++ WindowsService/StartUp.cmd
@@ -1,1 +1,1 @@
-"c:\Program Files\java\jdk-13.0.2\bin\java" -jar D:\code\dev-sxi\v3.0\trunk\xlayer-api\target\xlayerrestapi-3.0P2.jar --spring.config.location=file:///d:/code/XLayerAPI/WindowsService/application.properties
+"c:\Program Files\java\jdk-13.0.2\bin\java" -jar D:\SXIUtils\XlayerAPI\XLayerApi.jar --spring.config.location=file:///C:/SXIUtils/XLayerAPI/application.properties
